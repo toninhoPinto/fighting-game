ab9eb74a4e7f69e401421e138b469da2d2a1cf47
Toninho <Antonio.m.pinto@ist.utl.pt>
2021-05-26 00:45:41 +0100

    fix add attack error

--- src/game_logic/items/item_effects.rs
+++ src/game_logic/items/item_effects.rs
@@ -6,15 +6,19 @@ pub fn add_attack(player: &mut Player, effect: &mut Effect) {
         "kick" => {player.character.kick_string_curr+=1;},
         "airborne punch" => {player.character.airborne_punch_string_curr+=1;},
         "airborne kick" => {player.character.airborne_kick_string_curr+=1;},
-        "launcher" => {player.character.directional_attacks_mask =  0b0001u32;},
-        "dropper" => {player.character.directional_attacks_mask =   0b0010u32;}, 
-        "dashing" => {player.character.directional_attacks_mask =   0b0100u32;},
-        "crash" => {player.character.directional_attacks_mask =     0b1000u32;},
+        "launcher" => {player.character.directional_attacks_mask |=  0b0001u32;},
+        "dropper" => {player.character.directional_attacks_mask |=   0b0010u32;}, 
+        "dashing" => {player.character.directional_attacks_mask |=   0b0100u32;},
+        "crash" => {player.character.directional_attacks_mask |=     0b1000u32;},
         _ => {},
     }
 }
 
 pub fn apply_lifesteal(player: &mut Player, effect: &mut Effect){
+    player.events.on_hit.push((lifesteal, effect.clone()));
+}
+
+pub fn apply_life_on_kill(player: &mut Player, effect: &mut Effect){
     player.events.on_hit.push((lifesteal, effect.clone()));
 }
 
